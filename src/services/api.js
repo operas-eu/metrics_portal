10c0326f641411e01425f856e0e486fa3e729593
Javier Arias <javier@arias.re>
2018-12-20 14:56:48 +0000

    dont check import

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import { stringify } from 'qs';
 import request from '@/utils/request';
 
+/* eslint-disable import/prefer-default-export */
 export async function queryMeasure(params) {
   return request(`/api/measures?${stringify(params)}`);
 }
