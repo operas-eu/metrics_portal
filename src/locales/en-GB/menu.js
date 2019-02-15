b16d97c86937ad60e639cc4d3f9fb20d8854b037
Javier Arias <javier@arias.re>
2019-02-15 16:40:36 +0000

    add items to menu

--- src/locales/en-GB/menu.js
+++ src/locales/en-GB/menu.js
@@ -2,6 +2,7 @@ export default {
   'menu.home': 'Home',
   'menu.measures': 'Measures',
   'menu.principles': 'Our Principles',
-  'menu.quickstart': 'Get Started',
   'menu.documentation': 'Documentation',
+  'menu.quickstart': 'Getting Started',
+  'menu.translationsrv': 'Identifier Translation Service'
 };
