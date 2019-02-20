157b2afffe34c32fe33a1fcf9f08f7daf43f75f4
Javier Arias <javier@arias.re>
2019-02-20 14:55:29 +0000

    get rid of link as its not compatible with dynamic routing

--- src/pages/Docs/Docs.js
+++ src/pages/Docs/Docs.js
@@ -1,6 +1,5 @@
 import React, { PureComponent, Suspense } from 'react';
 import { getLocale } from 'umi/locale';
-import Link from 'umi/link';
 import { Card, Col, Row } from 'antd';
 import ReactMarkdown from 'react-markdown';
 
@@ -9,7 +8,7 @@ import PageLoading from '@/components/PageLoading';
 import styles from './Docs.less';
 
 // Document paths and menu items are specified in data.js
-import { files, paths, menus } from './data.js';
+import { files, paths, menu } from './data.js';
 
 const menuColResponsiveProps = {
   xs: 24,
@@ -59,7 +58,7 @@ class Docs extends PureComponent {
                   <li className={this.state.pathname == entry.path
                                  ? styles.itemselected : styles.menuitem}
                       key={entry.path}>
-                    <Link to={entry.path}>{entry.name}</Link>
+                    <a href={entry.path}>{entry.name}</a>
                   </li>
               ))}
               </ul>
