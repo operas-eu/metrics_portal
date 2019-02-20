877f428302ac093e9f9c52bbe73ebc548d46260b
Javier Arias <javier@arias.re>
2019-02-20 15:58:40 +0000

    do not index exception pages

--- src/pages/Exception/403.js
+++ src/pages/Exception/403.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { formatMessage } from 'umi/locale';
 import Link from 'umi/link';
 import Exception from '@/components/Exception';
 
 const Exception403 = () => (
-  <Exception
-    type="403"
-    desc={formatMessage({ id: 'app.exception.description.403' })}
-    linkElement={Link}
-    backText={formatMessage({ id: 'app.exception.back' })}
-  />
+  <Fragment>
+    <meta name="robots" content="noindex" />
+    <Exception
+      type="403"
+      desc={formatMessage({ id: 'app.exception.description.403' })}
+      linkElement={Link}
+      backText={formatMessage({ id: 'app.exception.back' })}
+    />
+  </Fragment>
 );
 
 export default Exception403;
--- src/pages/Exception/500.js
+++ src/pages/Exception/500.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { formatMessage } from 'umi/locale';
 import Link from 'umi/link';
 import Exception from '@/components/Exception';
 
 const Exception500 = () => (
-  <Exception
-    type="500"
-    desc={formatMessage({ id: 'app.exception.description.500' })}
-    linkElement={Link}
-    backText={formatMessage({ id: 'app.exception.back' })}
-  />
+  <Fragment>
+    <meta name="robots" content="noindex" />
+    <Exception
+      type="500"
+      desc={formatMessage({ id: 'app.exception.description.500' })}
+      linkElement={Link}
+      backText={formatMessage({ id: 'app.exception.back' })}
+    />
+  </Fragment>
 );
 
 export default Exception500;
