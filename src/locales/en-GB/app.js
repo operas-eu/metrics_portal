98e3090e57c5fc4cf099e4310fc77715addd3b31
yoannspace <yoann.moranville@gmail.com>
2019-12-18 12:20:39 +0100

    Add AUP page for Metrics

--- src/locales/en-GB/app.js
+++ src/locales/en-GB/app.js
@@ -13,5 +13,21 @@ export default {
   'app.exception.back': 'Go home',
   'app.exception.description.403': "These are not the droids you're looking for.",
   'app.exception.description.404': 'This page does not exist',
-  'app.exception.description.500': "Sorry, it's not you, it's us."
+  'app.exception.description.500': "Sorry, it's not you, it's us.",
+  'app.use.policy.title': "Acceptable Use Policy and Conditions of Use",
+  'app.use.policy.text': "This Acceptable Use Policy and Conditions of Use (“AUP”) defines the rules and conditions that govern your access to and use (including transmission, processing, and storage of data) of the resources and services (“Services”) as granted by OPERAS for the purpose of measuring Metrics and Altmetrics of books hosted on various Open Access platforms.",
+  'app.use.policy.text.point1': "1. You shall only use the Services in a manner consistent with the purposes and limitations described above; you shall show consideration towards other users including by not causing harm to the Services; you have an obligation to collaborate in the resolution of issues arising from your use of the Services.",
+  'app.use.policy.text.point2': "2. You shall only use the Services for lawful purposes and not breach, attempt to breach, nor circumvent administrative or security controls.",
+  'app.use.policy.text.point3': "3. You shall respect intellectual property and confidentiality agreements.",
+  'app.use.policy.text.point4': "4. You shall protect your access credentials (e.g. passwords, private keys or multi-factor tokens); no intentional sharing is permitted.",
+  'app.use.policy.text.point5': "5. You shall keep your registered information correct and up to date.",
+  'app.use.policy.text.point6': "6. You shall promptly report known or suspected security breaches, credential compromise, or misuse to the security contact stated below; and report any compromised credentials to the relevant issuing authorities.",
+  'app.use.policy.text.point7': "7. Reliance on the Services shall only be to the extent specified by any applicable service level agreements listed below. Use without such agreements is at your own risk.",
+  'app.use.policy.text.point8': "8. Your personal data will be processed in accordance with the privacy statements referenced below.",
+  'app.use.policy.text.point9': "9. Your use of the Services may be restricted or suspended, for administrative, operational, or security reasons, without prior notice and without compensation.",
+  'app.use.policy.text.point10': "10. If you violate these rules, you may be liable for the consequences, which may include your account being suspended and a report being made to your home organisation or to law enforcement.",
+  'app.use.policy.email.global': "The administrative contact for this AUP is: metrics-service_at_operas-eu.org",
+  'app.use.policy.email.security': "The security contact for this AUP is: technical-support_at_operas.eu-org",
+  'app.use.policy.privacy.stmt': "The privacy statements are located at: <a href='/privacy-policy'>https://metrics.operas-eu.org/privacy-policy</a>",
+  'app.use.policy.based.on': "This AUP is based on '<a target='_blank' href='https://aarc-project.eu/wp-content/uploads/2019/03/AARC-I044-Implementers-Guide-to-the-WISE-Baseline-AUP.pdf'>AARC-I044: Implementers Guide to the WISE Baseline Acceptable Use Policy</a>'"
 };
